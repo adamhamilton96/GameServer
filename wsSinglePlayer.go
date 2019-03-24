ee1cd39a46fc532f030b77b2dff255597623d076
adam hamilton <a_hamilton@live.co.uk>
2019-03-24 19:54:00 +0000

    add name to score list

--- wsSinglePlayer.go
+++ wsSinglePlayer.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 )
 
 // Floaty Square
@@ -22,20 +21,17 @@ func floatySquareEchoHandler(w http.ResponseWriter, r *http.Request) {
 		} else if err = conn.WriteMessage(msgType, msg); err != nil {
 			return
 		} else {
-			num, _ := strconv.Atoi(string(msg))
-			fmt.Println(num)
-
+			fmt.Println(string(msg))
 			f, err := os.OpenFile("/home/haxxionlaptop/Documents/Code/Go/GameServer/txt/floatySquareScore.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 			if err != nil {
 				log.Fatal(err)
 			}
-			if _, err := f.Write([]byte(strconv.Itoa(num) + "\n")); err != nil {
+			if _, err := f.Write([]byte(string(msg) + "\n")); err != nil {
 				log.Fatal(err)
 			}
 			if err := f.Close(); err != nil {
 				log.Fatal(err)
 			}
-			f.Close()
 		}
 	}
 }
